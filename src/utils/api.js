bcbc96255732eb63cd0cfb97cfce836d0291a3e1
AmrHosam <amrhossam97@gmail.com>
2021-06-16 21:16:37 +0200

    fix: Pass the return value not the function itself to the promise

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,12 @@
 import { _getQuestions, _getUsers, _saveQuestion, _saveQuestionAnswer } from './_DATA.js'
 
 export function getInitialData(){
-    return Promise.all([_getUsers, _getQuestions]).then(([users, questions]) => ({
+    return Promise.all([_getUsers(), _getQuestions()]).then(([users, questions]) => ({
         users,
         questions
     }))
 }
+
 
 export function saveQuestion(question){
     return _saveQuestion(question)
