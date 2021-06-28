463f1d55dfc20c6c78f66bdbbcad47078bf066f2
AmrHosam <amrhossam97@gmail.com>
2021-06-29 00:15:26 +0200

    feat: Add save answer reducers

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,4 +1,4 @@
-import { RECIEVE_USERS } from '../actions/users'
+import { RECIEVE_USERS, RECIEVE_ANSWER, ADD_USER_QUESTION } from '../actions/users'
 
 export default function users(state = {}, action){
     switch(action.type){
@@ -6,6 +6,25 @@ export default function users(state = {}, action){
             return{
                 ...state,
                 ...action.users
+            }
+        case RECIEVE_ANSWER:
+            return{
+                ...state,
+                [action.authedUser]:{
+                    ...state[action.authedUser],
+                    answers:{
+                        ...state[action.authedUser].answers,
+                        [action.qid]:action.answer
+                    }
+                }
+            }
+        case ADD_USER_QUESTION:
+            return{
+                ...state,
+                [action.author]:{
+                    ...state[action.author],
+                    questions: state[action.author].questions.concat([action.qid])
+                }
             }
         default:
             return state
