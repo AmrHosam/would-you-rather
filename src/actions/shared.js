e4405760da3fa59c5e18cdd1dd39829bfabb6220
AmrHosam <amrhossam97@gmail.com>
2021-06-29 00:14:00 +0200

    feat: Add save question actions

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,11 +1,10 @@
 import { showLoading, hideLoading } from 'react-redux-loading'
-import { getInitialData } from '../utils/api'
-import { recieveUsers } from './users'
-import { recieveQuestions } from './questions'
+import { getInitialData, saveQuestionAnswer, saveQuestion } from '../utils/api'
+import { recieveUsers, recieveAnswer, addUserQuestion } from './users'
+import { recieveQuestions, recieveVotes, addNewQuestion } from './questions'
 import { setAuthedUser } from './authedUser'
 
-
-const AUTHED_ID = "tylermcginnis"
+const AUTHED_ID = ""
 
 export function handleInitialData(){
     return (dispatch) => {
@@ -16,5 +15,30 @@ export function handleInitialData(){
             dispatch(setAuthedUser(AUTHED_ID))
             dispatch(hideLoading())
         })
+    }
+}
+
+export function saveAnswer(info){
+    return (dispatch) => {
+        saveQuestionAnswer(info).then(() => {
+            dispatch(recieveAnswer(info))
+            dispatch(recieveVotes(info))
+        })
+    }
+}
+
+export function handleAddQuestion(optionOneText, optionTwoText){
+    return (dispatch, getState) => {
+        const { authedUser } = getState()
+
+        dispatch(showLoading())
+        return saveQuestion({ 
+            author: authedUser,
+            optionOneText,
+            optionTwoText,
+        }).then((formattedQuestion) => {
+            dispatch(addNewQuestion(formattedQuestion))
+            dispatch(addUserQuestion(formattedQuestion))
+        }).then(() => dispatch(hideLoading()))
     }
 }
--- src/actions/users.js
+++ src/actions/users.js
@@ -1,8 +1,30 @@
 export const RECIEVE_USERS = "RECIEVE_USERS"
+export const RECIEVE_ANSWER = "RECIEVE_ANSWER"
+export const ADD_USER_QUESTION = "ADD_USER_QUESTION"
+
 
 export function recieveUsers(users){
     return{
         type: RECIEVE_USERS,
         users,
+    }
+}
+
+export function recieveAnswer({ authedUser, qid, answer })
+{
+    return{
+        type: RECIEVE_ANSWER,
+        authedUser,
+        qid,
+        answer
+    }
+}
+
+export function addUserQuestion({ id, author })
+{
+    return{
+        type: ADD_USER_QUESTION,
+        qid: id,
+        author,
     }
 }
