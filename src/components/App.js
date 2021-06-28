db25148860c1daa12096fa00ff149ed1ad49516a
AmrHosam <amrhossam97@gmail.com>
2021-06-29 01:25:25 +0200

    feat: Add react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { BrowserRouter as Router, Route} from 'react-router-dom'
 import { connect } from 'react-redux'
 import LoadingBar from 'react-redux-loading'
 import { handleInitialData } from '../actions/shared'
 import Home from './Home'
+import NewQuestion from './NewQuestion'
+import Leaderboard from './Leaderboard'
+import Nav from './Nav'
+import QuestionPage from './QuestionPage'
+import SignIn from './SignIn'
+import NotFoundPage from './NotFoundPage' 
 
 class App extends Component {
   componentDidMount(){
@@ -10,19 +17,35 @@ class App extends Component {
   }
   render(){
     return (
-      <div className="App">
-        <LoadingBar/>
-        {this.props.loading === true
-          ? null
-          : <Home/> }
-      </div>
+      <Router>
+        <Fragment>
+          <LoadingBar/>
+          <div className="App">
+            <Nav/>
+            {this.props.loading === true
+              ? null
+              : 
+              this.props.loggedout === true
+                ? <SignIn/>
+              :
+              <div>
+                <Route path="/" exact component={Home} />
+                <Route path='/question/:id' component={QuestionPage} />
+                <Route path="/add" component={NewQuestion} />
+                <Route path="/leaderboard" component={Leaderboard}/>
+                <Route path="/404" component={NotFoundPage}/>
+              </div>}
+          </div>
+        </Fragment>
+      </Router>
     );
   }
 }
 
 function mapStateToProps({ authedUser }){
   return{
-    loading: authedUser===null
+    loading: authedUser===null,
+    loggedout: authedUser === ""
   }
 }
 
